62cf4a28b47487aca86e75eecb6ba5c4eeeab45c
Mosi8 <mauriziomosi8@gmail.com>
2022-01-17 18:02:51 +0100

    completo con bonus

---
+++ myscript.js
@@ -0,0 +1,55 @@
+let app = new Vue ({
+    el : '#todolist',
+    data : {
+        nuovaTodo : "",
+        lista : [
+            {
+                'testo' : 'Fare la spesa',
+                'done' : false
+            },
+            {
+                'testo' : 'Andare dal gommista',
+                'done' : false
+            },
+            {
+                'testo' : 'Comprare un pc',
+                'done' : false
+            },
+            {
+                'testo' : 'Telefonare al capo',
+                'done' : false
+            }
+        ],
+
+    },
+    methods : {
+        addTodo : function () {
+            let newTask = {
+                'testo' : this.nuovaTodo,
+                'done' : false
+            } 
+            if(!this.nuovaTodo == ""){
+            this.lista.push(newTask);
+            }
+            this.nuovaTodo = "";
+        },
+        completato : function (elem) {
+            if(elem.done == false){
+                elem.done = true;
+            }else{
+                elem.done = false;
+            }
+        },
+        barrato : function (elem) {
+            if(elem.done == true){
+                return 'ms_barrato'
+            }
+            return ''
+        },
+        cancella : function (ogg) {
+            this.lista.splice(ogg,1)
+        }
+    },
+
+} 
+);
